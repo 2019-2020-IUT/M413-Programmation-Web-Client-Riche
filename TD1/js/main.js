04efba8ccc1d8c4058905bf51b6ef97b83ea545e
JunkJumper <jose.srifi@gmail.com>
2020-01-22 17:02:42 +0100

    defTitre1 ok

--- TD1/js/main.js
+++ TD1/js/main.js
@@ -5,8 +5,10 @@ function init(){
 	defTitre1();
 }
 
-function defTitre1(){
-	console.log(defTitre1());
+function defTitre1() {
+    if(document.title = document.getElementById("titre").innerText) {
+        console.log("<title> set : Ok Everypony !");
+    }
 }
 
 function sayHello() {
