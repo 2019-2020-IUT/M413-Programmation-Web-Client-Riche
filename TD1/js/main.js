e5d959e93d91c74c750dd6ac85e18a3e61975eff
JunkJumper <jose.srifi@gmail.com>
2020-01-22 17:27:33 +0100

    partie exercice 2

--- TD1/js/main.js
+++ TD1/js/main.js
@@ -2,12 +2,21 @@ console.log("JavaScript is linked");
 
 function init(){
     sayHello();
-	defTitre1();
+	defTitre2();
 }
 
 function defTitre1() {
     if(document.title = document.getElementById("titre").innerText) {
         console.log("<title> set : Ok Everypony !");
+    }
+}
+
+function defTitre2() {
+    var firstH2 = document.getElementsByTagName("h2");
+    if(firstH2 != null) {
+        document.title =firstH2[0].innerText;
+        console.log("<title> set : Ok Everypony !");
+
     }
 }
 
