b51378e29a44b0a0eecdc45d2c42165bc2a113f3
JunkJumper <jose.srifi@gmail.com>
2020-01-22 16:40:43 +0100

    Exercice 1 16h40

---
+++ TD1/js/main.js
@@ -0,0 +1,14 @@
+console.log("JavaScript is linked");
+
+function init(){
+    sayHello();
+	defTitre1();
+}
+
+function defTitre1(){
+	console.log(defTitre1());
+}
+
+function sayHello() {
+    console.log("Hello Everypony !");
+}
