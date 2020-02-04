0a2643231eaa7acf5e5ba067abe13c4dab2346c5
JunkJumper <jose.srifi@gmail.com>
2020-02-04 18:15:16 +0100

    "bak 18h15"

--- TD2/js/main.js
+++ TD2/js/main.js
@@ -1,10 +1,14 @@
-function initSelection() {
-    document.body.addEventListener("click", selection);
+console.log("JavaScript is linked"); /**Cette méthode permet de vérifier que Javascript est bien activé **/
+
+function init() {
+    //sayHello();
+    document.body.addEventListener("click", sayHello); //vérification que le listener est ok
 }
 
-function selection(event) {
-    console.log(event.target.tagName);
-    var element = event.target;
-    element.classList.toggle("pink");
+function sayHello() { /**Cette méthode permet de vérifier que la console de log est activée. **/
+    console.log("Hello Everypony !");
+}
 
+function initSelection() {
+    sayHello();
 }
