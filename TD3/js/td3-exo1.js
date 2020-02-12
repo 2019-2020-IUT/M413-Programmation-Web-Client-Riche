5017da42b5e229ab742a3a9d704b0138f7dbaa60
JunkJumper <jose.srifi@gmail.com>
2020-02-12 17:25:20 +0100

    17h25

--- TD3/js/td3-exo1.js
+++ TD3/js/td3-exo1.js
@@ -3,16 +3,25 @@ console.log("Chargement...");
 
 function init() {
     console.log("Initialisation...");
-    var div = document.querySelectorAll('.case');
+    var div = document.querySelectorAll(".case");
 
     for (var i = 0; i < div.length; i++) {
         div[i].addEventListener("click", selection);
     }
 
-    //console.log(grille);
 }
 
 function selection(event) {
-    console.log(event.currentTarget.tagName);
-    var e = event.target;
-}
+
+    var maCase = event.currentTarget;
+    var vide = document.body.querySelector(".case.vide");
+
+    if ((maCase.offsetTop == vide.offsetTop && Math.abs(maCase.offsetLeft - vide.offsetLeft == 102)) ||
+        (maCase.offsetLeft == vide.offsetLeft && Math.abs(maCase.offsetTop - vide.offsetTop == 102)) {
+            maCase.setAttribute("class", ".case.vide");
+            vide.setAttribute("class", ".case");
+        }
+
+        console.log(event.currentTarget.innerText);
+        var e = event.target;
+    }
